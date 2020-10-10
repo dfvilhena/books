f521c2306cbdba4b121f7d22153f5f9922461f74
dfvilhena <dvilhena.n@gmail.com>
2020-10-10 14:40:10 +0100

    diff df, device-width added

--- book_list_2_html.py
+++ book_list_2_html.py
@@ -5,21 +5,28 @@ filePath = "D:\\Documents\\Lista de Livros.xlsx"
 sheet_list = ["Livros", "Banda Desenhada", "Coleções BD", "Manuais", "eBooks"]
 
 df_books = pd.read_excel(filePath, sheet_name=sheet_list[0])
+df_bd = pd.read_excel(filePath, sheet_name=sheet_list[1])
+df_collect = pd.read_excel(filePath, sheet_name=sheet_list[2])
+df_textbook = pd.read_excel(filePath, sheet_name=sheet_list[3])
+
+df_list = [df_books, df_bd, df_collect, df_textbook]
 
 cols = df_books.columns.values
 
 cols = [cols[0], cols[1], cols[2], cols[3]]
 
+i = 0
 
-df_books = df_books[cols]
-df_books = df_books.fillna("-")
+for df in df_list:
+    df = df[cols]
+    df = df.fillna("-")
+    
+    html = df.to_html(index=False)
 
-print(df_books.head(5))
+    fileName = "df_"+str(i)+".html"
 
-#df_books.to_html("test.html")
-
-html = df_books.to_html()
-
-with open("dataframe.html", "w", encoding="utf-8") as file:
-    file.writelines('<meta charset="UTF-8">\n')
-    file.write(html)
+    with open(fileName, 'w', encoding='utf-8') as f:
+        f.writelines('<meta charset="UTF-8">\n')
+        f.writelines('<meta name="viewport" content="width=device-width, initial-scale=1.0">\n') 
+        f.write(html)
+    i += 1
